77328fd4fb88419089b9c37eac18f6fc3c688e43
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-05-01 14:03:46 -0700

    change markers for annotations

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -18,17 +18,36 @@ struct ContentView: View {
         )
     )
     
+    @State private var selectedPlace: Location?
+    
     var body: some View {
         MapReader { proxy in
             Map(initialPosition: startPosition) {
                 ForEach(locations) { location in
-                    Marker(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                            .onLongPressGesture {
+                                selectedPlace = location
+                            }
+                    }
                 }
             }
             .onTapGesture { position in
                 if let coordinate = proxy.convert(position, from: .local) {
                     let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
                     locations.append(newLocation)
+                }
+            }
+            .sheet(item: $selectedPlace) { place in
+                EditView(location: place) { newLocation in
+                    if let index = locations.firstIndex(of: place) {
+                        locations[index] = newLocation
+                    }
                 }
             }
         }
---
+++ BucketList/Results.swift
@@ -0,0 +1,22 @@
+//
+//  Results.swift
+//  BucketList
+//
+//  Created by Edwin on 5/1/24.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
