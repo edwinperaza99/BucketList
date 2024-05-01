263918baa49e50fb165c22189c33458f0ec76b3c
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-05-01 14:51:12 -0700

    adopt MVVM model

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -9,8 +9,7 @@ import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    @State private var locations = [Location]()
-    
+    @State private var viewModel = ViewModel()
     let startPosition = MapCameraPosition.region(
         MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
@@ -18,12 +17,10 @@ struct ContentView: View {
         )
     )
     
-    @State private var selectedPlace: Location?
-    
     var body: some View {
         MapReader { proxy in
             Map(initialPosition: startPosition) {
-                ForEach(locations) { location in
+                ForEach(viewModel.locations) { location in
                     Annotation(location.name, coordinate: location.coordinate) {
                         Image(systemName: "star.circle")
                             .resizable()
@@ -32,22 +29,18 @@ struct ContentView: View {
                             .background(.white)
                             .clipShape(.circle)
                             .onLongPressGesture {
-                                selectedPlace = location
+                                viewModel.selectedPlace = location
                             }
                     }
                 }
             }
             .onTapGesture { position in
                 if let coordinate = proxy.convert(position, from: .local) {
-                    let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
-                    locations.append(newLocation)
+                    viewModel.addLocation(at: coordinate)
                 }
-            }
-            .sheet(item: $selectedPlace) { place in
-                EditView(location: place) { newLocation in
-                    if let index = locations.firstIndex(of: place) {
-                        locations[index] = newLocation
-                    }
+            }            .sheet(item: $viewModel.selectedPlace) { place in
+                EditView(location: place) {
+                    viewModel.update(location: $0)
                 }
             }
         }
